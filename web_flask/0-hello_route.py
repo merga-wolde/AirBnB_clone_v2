752dbf3a306d8e253c315d2f82bc1881c6520ea4
merga wolde <49088728+merga-wolde@users.noreply.github.com>
2024-07-31 10:19:59 -0700

    hello route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+file: 0-hello_route.py
+desc: This module runs a simple flask app.
+
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB! from the root path"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
